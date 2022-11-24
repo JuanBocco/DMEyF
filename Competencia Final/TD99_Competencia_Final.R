77925df00a8e7f07409a7c83052c2f5cfd0cb06d
JuanBocco <juan.bocco@gmail.com>
2022-11-24 00:15:54 -0300

    version final actualizada

--- Competencia Final/TD99_Competencia_Final.R
+++ Competencia Final/TD99_Competencia_Final.R
@@ -1,8 +1,3 @@
-#Necesita para correr en Google Cloud
-# 128 GB de memoria RAM
-# 256 GB de espacio en el disco local
-#   8 vCPU
-
 # ZZ final que necesita de UNDERSAMPLING
 
 #limpio la memoria
@@ -12,16 +7,25 @@ gc()             #garbage collection
 require("data.table")
 
 require("lightgbm")
+require("primes")
 
 #Parametros del script
-PARAM  <- list()
-PARAM$experimento  <- "JJ120_semillerio_Final"
-PARAM$exp_input  <- "HT9420_FINAL"
+PARAM <- list()
+PARAM$experimento <- "JJ120_semillerio_FINAL"
+PARAM$exp_input <- "HT9420_FINAL"
 
-PARAM$modelos  <- 1 #este lo fui cambiando en distintas VM
+# PARAM$modelos  <- 2
+PARAM$modelo <- 1 # se usa el mejor de la OB, pero a futuro podria variar esto
+PARAM$semilla_primos <- 888809
+PARAM$semillerio <- 100 # ¿De cuanto será nuestro semillerio?
+PARAM$indice_inicio_semilla <- 1
+PARAM$indice_fin_semilla <- 20
 # FIN Parametros del script
 
-ksemilla  <- 888809
+# genero un vector de una cantidad de PARAM$semillerio  de semillas,  buscando numeros primos al azar
+primos <- generate_primes(min = 100000, max = 1000000) # genero TODOS los numeros primos entre 100k y 1M
+set.seed(PARAM$semilla_primos) # seteo la semilla que controla al sample de los primos
+ksemillas <- sample(primos)[1:PARAM$semillerio] # me quedo con  PARAM$semillerio primos al azar
 
 #------------------------------------------------------------------------------
 options(error = function() { 
@@ -62,28 +66,60 @@ dataset[ , clase01 := ifelse( clase_ternaria %in% c("BAJA+1","BAJA+2"), 1, 0 )  
 
 campos_buenos  <- setdiff( colnames(dataset), c( "clase_ternaria", "clase01") )
 
+#tb_semillerio_proba <- dfuture[, list(numero_de_cliente, foto_mes)]
+#tb_semillerio_rank <- dfuture[, list(numero_de_cliente, foto_mes)]
+
+# Guardo las semillas Y EL ORDEN en que son usadas
+write.csv(ksemillas, file = "ksemillas.csv", row.names = FALSE)
 
 #genero un modelo para cada uno de las modelos_qty MEJORES iteraciones de la Bayesian Optimization
-for( i in  1:PARAM$modelos )
+for( ksemilla in ksemillas[PARAM$indice_inicio_semilla:PARAM$indice_fin_semilla] )
 {
-  parametros  <- as.list( copy( tb_log[ i ] ) )
+  
+  # optimización: si los archivos ya existen, puedo hacer skip de esta semilla
+  nom_submit <- paste0(
+    PARAM$experimento,
+    "_",
+    sprintf("%d", ksemilla),
+    ".csv"
+  )
+  
+  nom_submit_rank <- paste0(
+    PARAM$experimento,
+    "_",
+    sprintf("%d", ksemilla),
+    "_rank.csv"
+  )
+  
+  nom_resultados <- paste0(
+    PARAM$experimento,
+    "_",
+    sprintf("M%d", PARAM$modelo),
+    "_",
+    sprintf("S%d_S%d", PARAM$indice_inicio_semilla, PARAM$indice_fin_semilla),
+    "_",
+    sprintf("%d", ksemilla),
+    "_resultados.csv"
+  )
+  
+  # Salteo las semillas ya procesadas
+  if (file.exists(nom_submit) && file.exists(nom_submit_rank) && file.exists(nom_resultados)) {
+    next # si, podría ser mas sofisticado, pero queda para el refactor
+  }
+  
+  message("procesando semilla ", ksemilla) # un poco de debug
+  parametros <- as.list(copy(tb_log[PARAM$modelo]))
   iteracion_bayesiana  <- parametros$iteracion_bayesiana
   
-  arch_modelo  <- paste0( "modelo_" ,
-                          sprintf( "%02d", i ),
-                          "_",
-                          sprintf( "%03d", iteracion_bayesiana ),
-                          ".model" )
-  
-  
+  message("Creando dataset ")
+  timestamp()
   #creo CADA VEZ el dataset de lightgbm
   dtrain  <- lgb.Dataset( data=    data.matrix( dataset[ , campos_buenos, with=FALSE] ),
                           label=   dataset[ , clase01],
                           weight=  dataset[ , ifelse( clase_ternaria %in% c("BAJA+2"), 1.0000001, 1.0)],
                           free_raw_data= FALSE
   )
-  
-  ganancia  <- parametros$ganancia
+  timestamp()
   
   #elimino los parametros que no son de lightgbm
   parametros$experimento  <- NULL
@@ -113,76 +149,64 @@ for( i in  1:PARAM$modelos )
   
   #genero el modelo entrenando en los datos finales
   set.seed( parametros$seed )
+  message("Entrenando el final model")
+  timestamp()
   modelo_final  <- lightgbm( data= dtrain,
                              param=  parametros,
                              verbose= -100 )
+  timestamp()
   
-  #grabo el modelo, achivo .model
-  lgb.save( modelo_final,
-            file= arch_modelo )
-  
-  #creo y grabo la importancia de variables
-  tb_importancia  <- as.data.table( lgb.importance( modelo_final ) )
-  fwrite( tb_importancia,
-          file= paste0( "impo_", 
-                        sprintf( "%02d", i ),
-                        "_",
-                        sprintf( "%03d", iteracion_bayesiana ),
-                        ".txt" ),
-          sep= "\t" )
-  
-  
+  message("Prediciendo")
+  timestamp()
   #genero la prediccion, Scoring
   prediccion  <- predict( modelo_final,
                           data.matrix( dfuture[ , campos_buenos, with=FALSE ] ) )
+  timestamp()
   
   tb_prediccion  <- dfuture[  , list( numero_de_cliente, foto_mes ) ]
   tb_prediccion[ , prob := prediccion ]
+  #hago el rank de las probabilidades
+  tb_prediccion[, rank := frank(-prob, ties.method = "random")]
+  tb_prediccion_rank <- data.table(tb_prediccion[, list(numero_de_cliente, foto_mes, rank)])
+  colnames(tb_prediccion_rank) <- c("numero_de_cliente", "foto_mes", "prediccion")
   
-  
-  nom_pred  <- paste0( "pred_",
-                       sprintf( "%02d", i ),
-                       "_",
-                       sprintf( "%03d", iteracion_bayesiana),
-                       ".csv"  )
-  
-  fwrite( tb_prediccion,
-          file= nom_pred,
-          sep= "\t" )
-  
+  #guardo los resultados de la predicción, por cada registro su probabilidad y ranking
+  fwrite(tb_prediccion[, list(numero_de_cliente, foto_mes, prob, rank)],
+         file = nom_resultados,
+         sep = ","
+  )
   
   #genero los archivos para Kaggle
-  cortes  <- seq( from=  7000,
-                  to=   11000,
-                  by=     500 )
-  
+  cortes  <- seq( from=  6000,
+                  to=    12000,
+                  by=        1000 )
   
   setorder( tb_prediccion, -prob )
+  setorder(tb_prediccion_rank, prediccion) # Esto es un ranking, entonces de menor a mayor
   
   for( corte in cortes )
   {
     tb_prediccion[  , Predicted := 0L ]
     tb_prediccion[ 1:corte, Predicted := 1L ]
     
-    nom_submit  <- paste0( PARAM$experimento, 
-                           "_",
-                           sprintf( "%02d", i ),
-                           "_",
-                           sprintf( "%03d", iteracion_bayesiana ),
-                           "_",
-                           sprintf( "%05d", corte ),
-                           ".csv" )
+    tb_prediccion_rank[, Predicted := 0L]
+    tb_prediccion_rank[1:corte, Predicted := 1L]
     
+    # Guardo el submit individual
     fwrite(  tb_prediccion[ , list( numero_de_cliente, Predicted ) ],
              file= nom_submit,
              sep= "," )
     
+    # Guardo el submit con rank
+    fwrite(tb_prediccion_rank[, list(numero_de_cliente, Predicted)],
+           file = nom_submit_rank,
+           sep = ","
+    )
+    
   }
-  
   
   #borro y limpio la memoria para la vuelta siguiente del for
   rm( tb_prediccion )
-  rm( tb_importancia )
   rm( modelo_final)
   rm( parametros )
   rm( dtrain )
